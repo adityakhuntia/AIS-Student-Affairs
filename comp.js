a2b0ff43cb68f234ada317be15e591afe662b9bf
adityakhuntia <adityakhuntia@yahoo.com>
2022-02-03 16:16:36 +0530

    competition server

--- comp.js
+++ comp.js
@@ -1,9 +1,68 @@
 console.log("You have entered Comp Space");
 
+const sheetID = '1hE4nQltQCLaY92d1KHHngOA3By9rzlMlYnWknqm1GgM';
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
+const sheetName = 'datareq';
+const query = encodeURIComponent('Select *');
+const url = `${base}&sheet=${sheetName}&tq=${query}`;
+const data = [];
+//document.addEventListener('DOMContentLoaded',init);//
 
-let fetchBtn = document.getElementById('fetchBtn');
+window.onload = function init(){
+    console.log('ready');
+    fetch(url)
+    .then(res => res.text())
+    .then(rep => {
+        const jsData = JSON.parse(rep.substring(47).slice(0,-2));
+        var main = jsData.table.rows
+        console.log(Object.keys(main).length)
+
+        for(let i=1;i<Object.keys(main).length;i++){
+            
+            var course =document.getElementById("crs_"+String(i));
+            var title = document.getElementById("ttl_"+String(i));
+            var desc = document.getElementById("dec_"+String(i));
+            var level = document.getElementById("lev_"+String(i));
+            course.style.display = "inline-flex";
+
+            title.innerHTML=(main[String(i)]["c"]["0"]["v"]);
+            desc.innerHTML=(main[String(i)]["c"]["1"]["v"]);
+            level.innerHTML=(main[String(i)]["c"]["2"]["v"])+"<br>"+(main[String(i)]["c"]["5"]["v"]);
+        }
+        //console.log(main["1"]["c"]["0"]["v"])
+    })
+
+
+
+    /*(for(let i=1;i<Object.keys(main).length;i++){
+        console.log(i)
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*window.onload = function getdata (){
+    console.log ("You have clicked button click handeler");
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','test.txt', true);
+    xhr.send();
+}*/
+
+/*let fetchBtn = document.getElementById('fetchBtn');
 fetchBtn.addEventListener('click',buttonclickhandeler)
-
 function buttonclickhandeler () {
     console.log ("You have clicked button click handeler");
 
@@ -37,7 +96,4 @@ function buttonclickhandeler () {
     params = `{"name":"test34sad545","salary":"123","age":"23"}`;
     xhr.send(params);
 
-    console.log("We are done!");
-
-}
-
+    console.log("We are done!");*/
